d05d5fefada4a8dce5b9c325e2d0d662772188cd
Matthew Ritter <mritter2283@gmail.com>
2018-10-14 12:10:38 -0400

    test three more API endpoints

--- index.js
+++ index.js
@@ -66,3 +66,47 @@ app.post("/api/posts", function(req, res) {
     });
   }
 });
+
+app.get("/api/posts/:id", function(req, res) {
+  db.collection(POSTS_COLLECTION).findOne(
+    { _id: new ObjectID(req.params.id) },
+    function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to get post");
+      } else {
+        res.status(200).json(doc);
+      }
+    }
+  );
+});
+
+app.put("/api/posts/:id", function(req, res) {
+  var updateDoc = req.body;
+  delete updateDoc._id;
+
+  db.collection(POSTS_COLLECTION).replaceOne(
+    { _id: new ObjectID(req.params.id) },
+    updateDoc,
+    function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to update post");
+      } else {
+        updateDoc._id = req.params.id;
+        res.status(200).json(updateDoc);
+      }
+    }
+  );
+});
+
+app.delete("/api/posts/:id", function(req, res) {
+  db.collection(POSTS_COLLECTION).deletOne(
+    { _id: new ObjectID(req.params.id) },
+    function(err, result) {
+      if (err) {
+        handleError(res, err.message, "Failed to delete post");
+      } else {
+        res.status(200).json(req.params.id);
+      }
+    }
+  );
+});
